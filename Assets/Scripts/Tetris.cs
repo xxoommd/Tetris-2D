78615f44540b407001d49edd20c37a006cef8f97
xxoommd <xxoommd@163.com>
2015-06-23 14:51:59 +0800

    Update

--- Assets/Scripts/Tetris.cs
+++ Assets/Scripts/Tetris.cs
@@ -1,21 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tetris : MonoBehaviour {
+public class Tetris : MonoBehaviour
+{
 
 	public GameObject brick;
-
 	protected GameObject[] bricks;
-
 	protected int brickCount;
 
-	protected virtual void Awake () {
+	protected virtual void Awake ()
+	{
 		brickCount = 4;
 		bricks = new GameObject[brickCount];
 
 		for (int i = 0; i < brickCount; i++) {
-			GameObject obj = Instantiate(brick, Vector3.zero, Quaternion.identity) as GameObject;
-			bricks[i] = obj;
+			GameObject obj = Instantiate (brick, Vector3.zero, Quaternion.identity) as GameObject;
+			bricks [i] = obj;
+			obj.transform.SetParent (transform);
 		}
+
+		transform.localPosition = new Vector3 (9.5f, 15f, 0f);
 	}
 }
