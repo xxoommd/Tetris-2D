3420960b20ffb501449158e2d4c851fe892024cc
xxoommd <xxoommd@163.com>
2015-06-29 18:49:42 +0800

    Update tetris.

--- Assets/Scripts/Tetris.cs
+++ Assets/Scripts/Tetris.cs
@@ -30,21 +30,17 @@ public abstract class Tetris : MonoBehaviour
 
 	// private
 	private int directionIndex;
-	private Vector3 nextPosition;
 	private bool moving = false;
 	private Vector3 fallPosition;
 	private bool falling = false;
 	private float xDir = 0;
 	private float yDir = 0;
-	private Vector3 targetPosition;
 
 	// Methods
 	protected virtual void Awake ()
 	{
 		InitialAllCoordinates ();
 
-		nextPosition = Vector3.zero;
-		targetPosition = Vector3.zero;
 		moving = false;
 
 		GameObject brick = brickTemplates [Random.Range (0, brickTemplates.Length)];
@@ -58,22 +54,21 @@ public abstract class Tetris : MonoBehaviour
 			obj.transform.localPosition = coordinates [directionIndex, i];
 			bricks [i] = obj;
 		}
-
-		nextPosition = transform.position;
 	}
 
-	protected virtual void Start () {
+	protected virtual void Start ()
+	{
 		xDir = transform.localPosition.x;
 	}
 
 	protected virtual void Update ()
 	{
-		if (Input.GetButtonUp ("Fire1")) {
+		if (Input.GetButtonUp ("Fire1") && CanTurn ()) {
 			Turn ();
 		}
 
 		int h = (int)Input.GetAxisRaw ("Horizontal");
-		if (!moving && CanMove(h)) {
+		if (!moving && CanMove (h)) {
 			if (h != 0) {
 				Move (h);
 			}
@@ -92,7 +87,7 @@ public abstract class Tetris : MonoBehaviour
 	{
 		Vector3 moveTranslate = Vector3.zero;
 		if (moving) {
-			if (Mathf.Abs(transform.localPosition.x - xDir) > 0.01f) {
+			if (Mathf.Abs (transform.localPosition.x - xDir) > 0.01f) {
 				moveTranslate.x = (xDir - transform.localPosition.x) * GameController.instance.tetrisMoveSpeed * Time.deltaTime;
 			} else {
 				Vector3 newPosition = transform.localPosition;
@@ -103,7 +98,7 @@ public abstract class Tetris : MonoBehaviour
 		}
 
 		if (falling) {
-			if (Mathf.Abs(transform.localPosition.y - yDir) > 0.01f) {
+			if (Mathf.Abs (transform.localPosition.y - yDir) > 0.01f) {
 				moveTranslate.y = (yDir - transform.localPosition.y) * GameController.instance.tetrisFallSpeed * Time.deltaTime;
 			} else {
 				Vector3 newPosition = transform.localPosition;
@@ -118,6 +113,54 @@ public abstract class Tetris : MonoBehaviour
 		}
 	}
 
+	protected void Turn ()
+	{
+		directionIndex = NextDirectionIndex ();
+		
+		for (int i = 0; i < 4; i++) {
+			GameObject brick = bricks [i];
+			brick.transform.localPosition = coordinates [directionIndex, i];
+		}
+
+		Boundary by = boundary [directionIndex];
+		transform.position = RevisedPosition (transform.position, by);
+	}
+
+	protected bool CanTurn ()
+	{
+		Boundary nextBound = boundary [NextDirectionIndex ()];
+
+		GameObject tempObj = new GameObject ();
+		tempObj.transform.SetParent (gameObject.transform.parent);
+		tempObj.transform.position = RevisedPosition (transform.position, nextBound);
+
+		Vector2[] brickPositions = new Vector2[4];
+
+		bool isOK = true;
+		for (int i = 0; i < 4; i++) {
+			brickPositions[i] = tempObj.transform.TransformPoint(coordinates[NextDirectionIndex(), i]);
+
+			int x = (int)brickPositions[i].x;
+			int y = (int)brickPositions[i].y;
+			if (GameController.instance.boardScript.brickBoxes[x, y] != null) {
+				isOK = false;
+			}
+		}
+
+		Destroy(tempObj);
+		if (isOK) {
+			return true;
+		} else {
+			// TODO
+			return false;
+		}
+	}
+
+	private int NextDirectionIndex ()
+	{
+		return (directionIndex + 1) % directionSize;
+	}
+
 	protected void Move (int h)
 	{
 		moving = true;
@@ -127,7 +170,8 @@ public abstract class Tetris : MonoBehaviour
 		xDir = xDir > bound.xMax ? bound.xMax : xDir;
 	}
 
-	protected bool CanMove(int h) {
+	protected bool CanMove (int h)
+	{
 		foreach (GameObject brick in bricks) {
 			Vector3 pos = brick.transform.position;
 			int x = (int)pos.x + h;
@@ -172,7 +216,6 @@ public abstract class Tetris : MonoBehaviour
 
 	protected void TransformToBricksAndDestroy ()
 	{
-		WaitForSeconds (0.5f);
 		// Revise position
 		Vector3 newPosition = new Vector3 (xDir, yDir, 0f);
 		transform.localPosition = newPosition;
@@ -188,22 +231,10 @@ public abstract class Tetris : MonoBehaviour
 		GameController.instance.boardScript.CheckClear ();
 	}
 
-	protected void Turn ()
-	{
-		directionIndex = (directionIndex + 1) % directionSize;
-
-		for (int i = 0; i < 4; i++) {
-			GameObject brick = bricks [i];
-			brick.transform.localPosition = coordinates [directionIndex, i];
-		}
-
-		transform.position = RevisedPosition (transform.position);
-	}
-
-	private Vector3 RevisedPosition (Vector3 position)
+	private Vector3 RevisedPosition (Vector3 position, Boundary by)
 	{
 		Vector3 newPosition = position;
-		Boundary by = boundary [directionIndex];
+
 		newPosition.x = newPosition.x < by.xMin ? by.xMin : newPosition.x;
 		newPosition.x = newPosition.x > by.xMax ? by.xMax : newPosition.x;
 		newPosition.y = newPosition.y < by.yMin ? by.yMin : newPosition.y;
